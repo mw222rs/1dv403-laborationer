6d7be7c28fe6e8a6c2475fbb7ae051f5d0d717d3
Mattias Wikström <mw222rs@student.lnu.se>
2015-01-08 13:39:33 +0100

    changes

--- 5-pwd/scripts/lib/windows/ImageViewerWindow.js
+++ 5-pwd/scripts/lib/windows/ImageViewerWindow.js
@@ -2,23 +2,23 @@
 
 define(["Window", "ImageViewerWindow", "Mustache"], function(Window, ImageViewerWindow, Mustache){
     var ImageViewerWindow = function(title){
-        this._title = title;        
-        
+        this._title = title;
+
         this.isOpen = false;
-        
+
         this.imgUrl = "pics/picture.png";
     };
-    
+
     ImageViewerWindow.prototype = new Window();
-    
+
     ImageViewerWindow.prototype.getImages = function(){
-    
+
         var that = this;
-        
+
         var thumbWidth = [];
         var template = $("#images-template").html();
         var $windowMain = $(".new").find(".windowMain");
-        
+
         $.ajax({
             type: "GET",
             url: "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/",
@@ -28,15 +28,15 @@ define(["Window", "ImageViewerWindow", "Mustache"], function(Window, ImageViewer
                 $.each(parsed, function(i, image){
                     data.images.push({URL: image.URL, thumbURL: image.thumbURL});
                     thumbWidth.push(image.thumbWidth);
-                });
+                });                              
 
-                var rendered = Mustache.render(template, data);                    
+                var rendered = Mustache.render(template, data);
                 $($windowMain).html(rendered);
 
 
                 thumbWidth.sort().reverse();
                 $($windowMain).find(".imgBox").width(thumbWidth[0]+4);
-                $($windowMain).parent().find(".windowFooter").html("<p>"+that.timer("stop")+"</p>");                
+                $($windowMain).parent().find(".windowFooter").html("<p>"+that.timer("stop")+"</p>");
 
                 $($windowMain).find("a").click(function(e){
                     document.body.style.backgroundImage = "url(" + this.firstChild.alt + ")";
@@ -50,6 +50,6 @@ define(["Window", "ImageViewerWindow", "Mustache"], function(Window, ImageViewer
             }
         });
     };
-    
+
     return ImageViewerWindow;
 });
